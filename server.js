cbd1f4cdc4ca850756b98ac4da1effe317181f9f
unknown <paolo.delia99@gmail.com>
2020-03-20 16:33:32 +0100

    Majority of the Endpoints tested

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
